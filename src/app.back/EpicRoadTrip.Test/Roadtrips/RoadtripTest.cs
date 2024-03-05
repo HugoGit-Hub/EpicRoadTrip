f08796b5eb08e9eb63360949d3ed250e80dab5eb
hugo <hugo.decuq.34@icloud.com>
2024-03-05 09:41:27 +0100

    HDO: Roadtrip entity and his tests

---
+++ src/app.back/EpicRoadTrip.Test/Roadtrips/RoadtripTest.cs
@@ -0,0 +1,56 @@
+﻿using EpicRoadTrip.Domain.Roadtrips.Exceptions;
+using EpicRoadTrip.Domain.Roadtrips;
+
+namespace EpicRoadTrip.Test.Roadtrips;
+
+[TestClass]
+public class RoadtripTest
+{
+    private const int Id = 1;
+    private const double PositiveBudget = 1000.0;
+    private readonly DateTime _dateTimeNow = DateTime.Now;
+
+    [TestMethod]
+    public void CreateRoadtrip_ValidParameters_CreatesRoadtrip()
+    {
+        // Arrange
+        const int userId = 1;
+        DateTime? endDate = DateTime.Now.AddDays(7);
+
+        // Act
+        var roadtrip = Roadtrip.Create(Id, PositiveBudget, _dateTimeNow, endDate, userId);
+
+        // Assert
+        Assert.AreEqual(Id, roadtrip.Id);
+        Assert.AreEqual(PositiveBudget, roadtrip.Budget);
+        Assert.AreEqual(_dateTimeNow, roadtrip.StartDate);
+        Assert.AreEqual(endDate, roadtrip.EndDate);
+        Assert.AreEqual(userId, roadtrip.UserId);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(RoadtripNotValidBudgetException))]
+    public void CreateRoadtrip_InvalidBudget_ThrowsException()
+    {
+        // Arrange
+        const double budget = -1000.0;
+        DateTime? endDate = DateTime.Now.AddDays(7);
+        const int userId = 1;
+
+        // Act
+        Roadtrip.Create(Id, budget, _dateTimeNow, endDate, userId);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(RoadtripNotValidDatesException))]
+    public void CreateRoadtrip_InvalidDates_ThrowsException()
+    {
+        // Arrange
+        var startDate = DateTime.Now.AddDays(7);
+        const int userId = 1;
+
+        // Act
+        Roadtrip.Create(Id, PositiveBudget, startDate, _dateTimeNow, userId);
+    }
+
+}

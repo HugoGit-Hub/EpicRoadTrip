6d58a62a39a6aef7e5743b232a2feb4baf791d88
hugo <hugo.decuq.34@icloud.com>
2024-03-05 14:10:58 +0100

    HDO: Hotel and his tests

---
+++ src/app.back/EpicRoadTrip.Domain/Hotels/Hotel.cs
@@ -0,0 +1,40 @@
+﻿using EpicRoadTrip.Domain.Institutions;
+
+namespace EpicRoadTrip.Domain.Hotels;
+
+public sealed class Hotel : Institution
+{
+    public int Id { get; }
+
+    private Hotel(
+        int id,
+        string name,
+        double? price,
+        string? phoneNumber,
+        string? email,
+        string address,
+        int cityId) 
+        : base(name, price, phoneNumber, email, address, cityId)
+    {
+        Id = id;
+    }
+
+    public static Hotel Create(
+        int id,
+        string name,
+        double price,
+        string phoneNumber,
+        string email,
+        string address,
+        int cityId)
+    {
+        return new Hotel(
+            id,
+            name,
+            price,
+            phoneNumber,
+            email,
+            address,
+            cityId);
+    }
+}

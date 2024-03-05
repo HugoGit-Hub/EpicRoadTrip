3a59864cddd783cb1e9c87b303ffb99a96ed356a
hugo <hugo.decuq.34@icloud.com>
2024-03-05 14:11:14 +0100

    HDO: Restaurant and his tests

---
+++ src/app.back/EpicRoadTrip.Test/Restaurants/RestaurantTest.cs
@@ -0,0 +1,54 @@
+﻿using EpicRoadTrip.Domain.Institutions.Exceptions;
+using EpicRoadTrip.Domain.Restaurants;
+
+namespace EpicRoadTrip.Test.Restaurants;
+
+[TestClass]
+public class RestaurantTest
+{
+    private const int Id = 1;
+    private const string Name = "Restaurant";
+    private const double Price = 100.0;
+    private const string PhoneNumber = "123456789";
+    private const string Email = "example@gmail.com";
+    private const string Address = "10 Rue de Rivoli";
+    private const int CityId = 1;
+
+    [TestMethod]
+    public void CreateRestaurant_ValidParameters_CreatesRestaurant()
+    {
+        // Act
+        var hotel = Restaurant.Create(Id, Name, Price, PhoneNumber, Email, Address, CityId);
+
+        // Assert
+        Assert.AreEqual(Id, hotel.Id);
+        Assert.AreEqual(Name, hotel.Name);
+        Assert.AreEqual(Price, hotel.Price);
+        Assert.AreEqual(PhoneNumber, hotel.PhoneNumber);
+        Assert.AreEqual(Email, hotel.Email);
+        Assert.AreEqual(Address, hotel.Address);
+        Assert.AreEqual(CityId, hotel.CityId);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InstitutionInvalidNameException))]
+    public void CreateRestaurant_WithInvalidName_ThrowException()
+    {
+        // Arrange
+        var invalidName = string.Empty;
+
+        // Act
+        Restaurant.Create(Id, invalidName, Price, PhoneNumber, Email, Address, CityId);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InstitutionInvalidAddressException))]
+    public void CreateRestaurant_WithInvalidAddress_ThrowException()
+    {
+        // Arrange
+        var invalidAddress = string.Empty;
+
+        // Act
+        Restaurant.Create(Id, Name, Price, PhoneNumber, Email, invalidAddress, CityId);
+    }
+}

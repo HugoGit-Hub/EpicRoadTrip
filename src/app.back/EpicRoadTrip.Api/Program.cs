c6412a5623767a0681d742845b23c5d8916c7d42
hugo <hugo.decuq.34@icloud.com>
2024-03-06 08:51:19 +0100

    HDO: Create Application project DependencyInjection

--- src/app.back/EpicRoadTrip.Api/Program.cs
+++ src/app.back/EpicRoadTrip.Api/Program.cs
@@ -1,9 +1,15 @@
+using EpicRoadTrip.Application;
+using EpicRoadTrip.Infrastructure;
+using EpicRoadTrip.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();

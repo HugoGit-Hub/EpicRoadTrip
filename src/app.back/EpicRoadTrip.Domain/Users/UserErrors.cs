b9783ac6d42abbc6d8a287a3688c325d4ad1d08a
hugo <hugo.decuq.34@icloud.com>
2024-03-07 09:11:37 +0100

    HDO: Change User entity creation

---
+++ src/app.back/EpicRoadTrip.Domain/Users/UserErrors.cs
@@ -0,0 +1,9 @@
+﻿using EpicRoadTrip.Domain.ErrorHandling;
+
+namespace EpicRoadTrip.Domain.Users;
+
+public static class UserErrors
+{
+    public static Error UserInvalidFormatException(Exception e) => new(
+               $"{nameof(User)}.{nameof(UserInvalidFormatException)}", $"The provided user is not in a valid format, {e}");
+}

d90cf9fb8748c3a1733ca25d674a3a8a04e7d7f9
hugo <hugo.decuq.34@icloud.com>
2024-03-07 12:28:56 +0100

    HDO: User Serivce / Repo / Erros

---
+++ src/app.back/EpicRoadTrip.Domain/Users/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using EpicRoadTrip.Domain.ErrorHandling;
+
+namespace EpicRoadTrip.Domain.Users;
+
+public interface IUserService
+{
+    public Task<Result<User>> Create(User user, CancellationToken cancellationToken);
+}
--- src/app.back/EpicRoadTrip.Domain/Users/UserErrors.cs
+++ src/app.back/EpicRoadTrip.Domain/Users/UserErrors.cs
@@ -4,6 +4,9 @@ namespace EpicRoadTrip.Domain.Users;
 
 public static class UserErrors
 {
-    public static Error UserInvalidFormatException(Exception e) => new(
-               $"{nameof(User)}.{nameof(UserInvalidFormatException)}", $"The provided user is not in a valid format, {e}");
+    public static Error UserInvalidFormatException(Exception e) => 
+        new($"{nameof(User)}.{nameof(UserInvalidFormatException)}", $"The provided user is not in a valid format, {e}");
+
+    public static Error ContextFailedToCreateUser(Exception e) =>
+        new($"{nameof(User)}.{nameof(ContextFailedToCreateUser)}", $"Something went wrong at user context creation, {e}");
 }

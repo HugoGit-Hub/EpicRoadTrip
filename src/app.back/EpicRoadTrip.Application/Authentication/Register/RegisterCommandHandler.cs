e85ed43b961d5729a4dbecd9127dbe57a7c825cc
hugo <hugo.decuq.34@icloud.com>
2024-03-07 09:15:30 +0100

    Test and create RegisterCommandHandler

---
+++ src/app.back/EpicRoadTrip.Application/Authentication/Register/RegisterCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using EpicRoadTrip.Domain.Authentications;
+using EpicRoadTrip.Domain.ErrorHandling;
+using EpicRoadTrip.Domain.Users;
+using MediatR;
+using System.Text.Json;
+
+namespace EpicRoadTrip.Application.Authentication.Register;
+
+public class RegisterCommandHandler(IAuthenticationService authenticationService, IUserService userService)
+    : IRequestHandler<RegisterCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(RegisterCommand command, CancellationToken cancellationToken)
+    {
+        var request = command.Request;
+        var isEmailAlreadyUse = await authenticationService.IsEmailAlreadyUse(request.Email, cancellationToken);
+        if (isEmailAlreadyUse)
+        {
+            return Result<string>.Failure(RegisterErrors.EmailAlreadyInUseError(request.Email));
+        }
+
+        var encryptedPassword = authenticationService.Encrypt(request.Password);
+        if (encryptedPassword.IsFailure)
+        {
+            return Result<string>.Failure(encryptedPassword.Error);
+        }
+
+        var hashedAndSaltedPassword = authenticationService.HashWithSalt(encryptedPassword.Value);
+        if (hashedAndSaltedPassword.IsFailure)
+        {
+            return Result<string>.Failure(hashedAndSaltedPassword.Error);
+        }
+
+        var user = User.Create(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            hashedAndSaltedPassword.Value,
+            request.Age,
+            request.Gender);
+        if (user.IsFailure)
+        {
+            return Result<string>.Failure(user.Error);
+        }
+
+        var createUser = await userService.Create(user.Value, cancellationToken);
+        if (createUser.IsFailure)
+        {
+            return Result<string>.Failure(createUser.Error);
+        }
+
+        var token = authenticationService.GenerateToken(user.Value);
+        if (token.IsFailure)
+        {
+            return Result<string>.Failure(token.Error);
+        }
+
+        var json = JsonSerializer.Serialize(new {Token = token.Value});
+
+        return Result<string>.Success(json);
+    }
+}
---
+++ src/app.back/EpicRoadTrip.Test/Application/Register/RegisterCommandHandlerTest.cs
@@ -0,0 +1,292 @@
+﻿using EpicRoadTrip.Application.Authentication.Register;
+using EpicRoadTrip.Domain.Authentications;
+using EpicRoadTrip.Domain.ErrorHandling;
+using EpicRoadTrip.Domain.Users;
+using Moq;
+
+namespace EpicRoadTrip.Test.Application.Register;
+
+[TestClass]
+public class RegisterCommandHandlerTest
+{
+    private const string Email = "example@gmail.com";
+    private const string Password = "Password123";
+    private const string Firstname = "Bar";
+    private const string Lastname = "Foo";
+    private const int Age = 25;
+    private const bool Gender = true;
+
+    [TestMethod]
+    public async Task Handle_ReturnsSuccessResult_WhenAllServicesSucceed()
+    {
+        // Arrange
+        var mockAuthenticationService = new Mock<IAuthenticationService>();
+        var mockUserService = new Mock<IUserService>();
+        var request = new RegisterRequest
+        {
+            Email = Email,
+            Password = Password,
+            FirstName = Firstname,
+            LastName = Lastname,
+            Age = Age,
+            Gender = Gender
+        };
+
+        var command = new RegisterCommand(request);
+        mockAuthenticationService
+            .Setup(service => service.IsEmailAlreadyUse(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+        
+        mockAuthenticationService
+            .Setup(service => service.Encrypt(It.IsAny<string>()))
+            .Returns(Result<string>.Success("encryptedPassword"));
+        
+        mockAuthenticationService
+            .Setup(service => service.HashWithSalt(It.IsAny<string>()))
+            .Returns(Result<string>.Success("hashedAndSaltedPassword"));
+
+        mockUserService
+            .Setup(service => service.Create(It.IsAny<User>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result<User>.Success(It.IsAny<User>()));
+
+        mockAuthenticationService
+            .Setup(service => service.GenerateToken(It.IsAny<User>()))
+            .Returns(Result<string>.Success("token"));
+        
+        var handler = new RegisterCommandHandler(mockAuthenticationService.Object, mockUserService.Object);
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+    }
+
+    [TestMethod]
+    public async Task Handle_ReturnsFailureResult_WhenIsEmailUniqueFails()
+    {
+        // Arrange
+        var mockAuthenticationService = new Mock<IAuthenticationService>();
+        var mockUserService = new Mock<IUserService>();
+        var request = new RegisterRequest
+        {
+            Email = Email,
+            Password = Password,
+            FirstName = Firstname,
+            LastName = Lastname,
+            Age = Age,
+            Gender = Gender
+        };
+
+        var command = new RegisterCommand(request);
+        mockAuthenticationService
+            .Setup(service => service.IsEmailAlreadyUse(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(false);
+        
+        var handler = new RegisterCommandHandler(mockAuthenticationService.Object, mockUserService.Object);
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsTrue(result.IsFailure);
+    }
+
+    [TestMethod]
+    public async Task Handle_ReturnsFailureResult_WhenEncryptFails()
+    {
+        // Arrange
+        var mockAuthenticationService = new Mock<IAuthenticationService>();
+        var mockUserService = new Mock<IUserService>();
+        var request = new RegisterRequest
+        {
+            Email = Email,
+            Password = Password,
+            FirstName = Firstname,
+            LastName = Lastname,
+            Age = Age,
+            Gender = Gender
+        };
+
+        var command = new RegisterCommand(request);
+        mockAuthenticationService
+            .Setup(service => service.IsEmailAlreadyUse(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        mockAuthenticationService
+            .Setup(service => service.Encrypt(It.IsAny<string>()))
+            .Returns(Result<string>.Failure(It.IsAny<Error>()));
+
+        var handler = new RegisterCommandHandler(mockAuthenticationService.Object, mockUserService.Object);
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsTrue(result.IsFailure);
+    }
+
+    [TestMethod]
+    public async Task Handle_ReturnsFailureResult_WhenHashWithSaltFails()
+    {
+        // Arrange
+        var mockAuthenticationService = new Mock<IAuthenticationService>();
+        var mockUserService = new Mock<IUserService>();
+        var request = new RegisterRequest
+        {
+            Email = Email,
+            Password = Password,
+            FirstName = Firstname,
+            LastName = Lastname,
+            Age = Age,
+            Gender = Gender
+        };
+
+        var command = new RegisterCommand(request);
+        mockAuthenticationService
+            .Setup(service => service.IsEmailAlreadyUse(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        mockAuthenticationService
+            .Setup(service => service.Encrypt(It.IsAny<string>()))
+            .Returns(Result<string>.Success("encryptedPassword"));
+
+        mockAuthenticationService
+            .Setup(service => service.HashWithSalt(It.IsAny<string>()))
+            .Returns(Result<string>.Failure(It.IsAny<Error>()));
+
+        var handler = new RegisterCommandHandler(mockAuthenticationService.Object, mockUserService.Object);
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsTrue(result.IsFailure);
+    }
+
+    [TestMethod]
+    public async Task Handle_ReturnsFailureResult_WhenCreateUserFails()
+    {
+        // Arrange
+        var mockAuthenticationService = new Mock<IAuthenticationService>();
+        var mockUserService = new Mock<IUserService>();
+        var request = new RegisterRequest
+        {
+            Email = Email,
+            Password = Password,
+            FirstName = Firstname,
+            LastName = Lastname,
+            Age = -1,
+            Gender = Gender
+        };
+
+        var command = new RegisterCommand(request);
+        mockAuthenticationService
+            .Setup(service => service.IsEmailAlreadyUse(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        mockAuthenticationService
+            .Setup(service => service.Encrypt(It.IsAny<string>()))
+            .Returns(Result<string>.Success("encryptedPassword"));
+
+        mockAuthenticationService
+            .Setup(service => service.HashWithSalt(It.IsAny<string>()))
+            .Returns(Result<string>.Success("hashedAndSaltedPassword"));
+
+        var handler = new RegisterCommandHandler(mockAuthenticationService.Object, mockUserService.Object);
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsTrue(result.IsFailure);
+    }
+
+    [TestMethod]
+    public async Task Handle_ReturnsFailureResult_WhenCreateUserServiceFails()
+    {
+        // Arrange
+        var mockAuthenticationService = new Mock<IAuthenticationService>();
+        var mockUserService = new Mock<IUserService>();
+        var request = new RegisterRequest
+        {
+            Email = Email,
+            Password = Password,
+            FirstName = Firstname,
+            LastName = Lastname,
+            Age = Age,
+            Gender = Gender
+        };
+
+        var command = new RegisterCommand(request);
+        mockAuthenticationService
+            .Setup(service => service.IsEmailAlreadyUse(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        mockAuthenticationService
+            .Setup(service => service.Encrypt(It.IsAny<string>()))
+            .Returns(Result<string>.Success("encryptedPassword"));
+
+        mockAuthenticationService
+            .Setup(service => service.HashWithSalt(It.IsAny<string>()))
+            .Returns(Result<string>.Success("hashedAndSaltedPassword"));
+
+        mockUserService
+            .Setup(service => service.Create(It.IsAny<User>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result<User>.Failure(It.IsAny<Error>()));
+
+        var handler = new RegisterCommandHandler(mockAuthenticationService.Object, mockUserService.Object);
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsTrue(result.IsFailure);
+    }
+    
+    [TestMethod]
+    public async Task Handle_ReturnsFailureResult_WhenGenerateTokenFails()
+    {
+        // Arrange
+        var mockAuthenticationService = new Mock<IAuthenticationService>();
+        var mockUserService = new Mock<IUserService>();
+        var request = new RegisterRequest
+        {
+            Email = Email,
+            Password = Password,
+            FirstName = Firstname,
+            LastName = Lastname,
+            Age = Age,
+            Gender = Gender
+        };
+
+        var command = new RegisterCommand(request);
+        mockAuthenticationService
+            .Setup(service => service.IsEmailAlreadyUse(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        mockAuthenticationService
+            .Setup(service => service.Encrypt(It.IsAny<string>()))
+            .Returns(Result<string>.Success("encryptedPassword"));
+
+        mockAuthenticationService
+            .Setup(service => service.HashWithSalt(It.IsAny<string>()))
+            .Returns(Result<string>.Success("hashedAndSaltedPassword"));
+
+        mockUserService
+            .Setup(service => service.Create(It.IsAny<User>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result<User>.Success(It.IsAny<User>()));
+
+        mockAuthenticationService
+            .Setup(service => service.GenerateToken(It.IsAny<User>()))
+            .Returns(Result<string>.Failure(It.IsAny<Error>()));
+
+        var handler = new RegisterCommandHandler(mockAuthenticationService.Object, mockUserService.Object);
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsTrue(result.IsFailure);
+    }
+}

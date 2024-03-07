af1cb8200cd8b032c2121e9115bfa5eb088169c4
hugo <hugo.decuq.34@icloud.com>
2024-03-07 12:26:46 +0100

    HDO: Set dependencies

--- src/app.back/EpicRoadTrip.Application/DependencyInjection.cs
+++ src/app.back/EpicRoadTrip.Application/DependencyInjection.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using EpicRoadTrip.Application.Authentication;
+using EpicRoadTrip.Application.Options;
+using EpicRoadTrip.Application.Users;
+using EpicRoadTrip.Domain.Authentications;
+using EpicRoadTrip.Domain.Users;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EpicRoadTrip.Application;
 
@@ -6,5 +11,13 @@ public static class DependencyInjection
 {
     public static void AddApplication(this IServiceCollection services)
     {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(AssemblyMarker.Assembly));
+        services.ConfigureServices();
+    }
+    
+    private static void ConfigureServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddScoped<IUserService, UserService>();
     }
 }

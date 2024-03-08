1aa0d9c3fc0af658f42b712941e7d9f60db7bf02
hugo <hugo.decuq.34@icloud.com>
2024-03-08 14:04:13 +0100

    HDO: Create Tests

--- src/app.back/EpicRoadTrip.Test/Api/AuthenticationControllerTest.cs
+++ src/app.back/EpicRoadTrip.Test/Api/AuthenticationControllerTest.cs
@@ -1,4 +1,6 @@
 ﻿using EpicRoadTrip.Api.Controllers;
+using EpicRoadTrip.Application.Authentications;
+using EpicRoadTrip.Application.Authentications.Logins;
 using EpicRoadTrip.Application.Authentications.Registers;
 using EpicRoadTrip.Domain.ErrorHandling;
 using MediatR;
@@ -41,6 +43,63 @@ public class AuthenticationControllerTest
 
         // Act
         var result = await controller.Register(It.IsAny<RegisterRequest>(), CancellationToken.None);
+
+        // Assert
+        Assert.IsInstanceOfType(result.Result, typeof(BadRequestObjectResult));
+        Assert.AreEqual(It.IsAny<string>(), ((BadRequestObjectResult)result.Result).Value);
+    }
+
+    [TestMethod]
+    public async Task Login_ReturnsOkResult_WhenLoginCommandSucceeds()
+    {
+        // Arrange
+        var mockSender = new Mock<ISender>();
+        mockSender
+            .Setup(sender => sender.Send(It.IsAny<LoginCommand>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result<string>.Success(It.IsAny<string>()));
+
+        var controller = new AuthenticationController(mockSender.Object);
+
+        // Act
+        var result = await controller.Login(It.IsAny<LoginRequest>(), CancellationToken.None);
+
+        // Assert
+        Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
+        Assert.AreEqual(It.IsAny<string>(), ((OkObjectResult)result.Result).Value);
+    }
+
+    [TestMethod]
+    public async Task Login_ReturnsUnauthorizedResult_WhenLoginCommandFailsWithInvalidEmailOrPasswordError()
+    {
+        // Arrange
+        var mockSender = new Mock<ISender>();
+        mockSender
+            .Setup(sender => sender.Send(It.IsAny<LoginCommand>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result<string>.Failure(AuthenticationErrors.InvalidEmailOrPasswordError));
+
+        var controller = new AuthenticationController(mockSender.Object);
+
+        // Act
+        var result = await controller.Login(It.IsAny<LoginRequest>(), CancellationToken.None);
+
+        // Assert
+        Assert.IsInstanceOfType(result.Result, typeof(UnauthorizedObjectResult));
+        Assert.AreEqual(AuthenticationErrors.InvalidEmailOrPasswordError, ((UnauthorizedObjectResult)result.Result).Value);
+    }
+
+    [TestMethod]
+    public async Task Login_ReturnsBadRequestResult_WhenLoginCommandFails()
+    {
+        // Arrange
+        var mockSender = new Mock<ISender>();
+        mockSender
+            .Setup(sender => sender.Send(It.IsAny<LoginCommand>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result<string>.Failure(It.IsAny<Error>()));
+
+        var controller = new AuthenticationController(mockSender.Object);
+
+        // Act
+        var result = await controller.Login(It.IsAny<LoginRequest>(), CancellationToken.None);
 
         // Assert
         Assert.IsInstanceOfType(result.Result, typeof(BadRequestObjectResult));

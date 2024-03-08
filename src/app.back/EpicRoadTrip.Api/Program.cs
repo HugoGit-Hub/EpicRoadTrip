df3e71465d4f8ed3d847d46a4dcff5d261221818
hugo <hugo.decuq.34@icloud.com>
2024-03-08 15:27:13 +0100

    HDO: Adapt middle ware pipeline

--- src/app.back/EpicRoadTrip.Api/Program.cs
+++ src/app.back/EpicRoadTrip.Api/Program.cs
@@ -1,38 +1,4 @@
-using EpicRoadTrip.Application;
-using EpicRoadTrip.Infrastructure;
-using EpicRoadTrip.Infrastructure.Context;
-using Microsoft.EntityFrameworkCore;
+using EpicRoadTrip.Api.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
-
-var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<EpicRoadTripContext>();
-    if (context.Database.GetPendingMigrations().Any())
-    {
-        context.Database.Migrate();
-    }
-}
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+builder.Configure();

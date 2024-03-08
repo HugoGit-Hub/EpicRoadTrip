8cf9a33ccd7044a8303d546c0b9866c31f9c3652
hugo <hugo.decuq.34@icloud.com>
2024-03-08 09:51:34 +0100

    HDO: Fix typos

--- src/app.back/EpicRoadTrip.Application/Authentications/Registers/RegisterRequest.cs
+++ src/app.back/EpicRoadTrip.Application/Authentications/Registers/RegisterRequest.cs
@@ -3,9 +3,14 @@
 public record RegisterRequest
 {
     public required string Email { get; init; }
+
     public required string Password { get; init; }
+
     public required string FirstName { get; init; }
+
     public required string LastName { get; init; }
+
     public int Age { get; init; }
+
     public bool Gender { get; init; }
 }

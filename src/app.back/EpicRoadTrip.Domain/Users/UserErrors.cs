59e30fe42491a2ad4dde03bd54fc18f337337ce0
hugo <hugo.decuq.34@icloud.com>
2024-03-08 16:19:45 +0100

    HDO: Remove unused user error

--- src/app.back/EpicRoadTrip.Domain/Users/UserErrors.cs
+++ src/app.back/EpicRoadTrip.Domain/Users/UserErrors.cs
@@ -7,9 +7,6 @@ public static class UserErrors
     public static Error UserNotFoundByEmailError => 
         new($"{nameof(User)}.{nameof(UserNotFoundByEmailError)}", "The user was not found by the provided email");
 
-    public static Error UserInvalidFormatException(Exception e) => 
-        new($"{nameof(User)}.{nameof(UserInvalidFormatException)}", $"The provided user is not in a valid format, {e}");
-
     public static Error ContextFailedToCreateUser(Exception e) =>
         new($"{nameof(User)}.{nameof(ContextFailedToCreateUser)}", $"Something went wrong at user context creation, {e}");
 }

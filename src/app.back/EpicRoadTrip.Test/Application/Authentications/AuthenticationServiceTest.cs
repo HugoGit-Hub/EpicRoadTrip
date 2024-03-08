798e21d12b7090e1f154e4740b8ff669a21f2e2c
hugo <hugo.decuq.34@icloud.com>
2024-03-08 09:38:18 +0100

    HDO: AuthenticationService unit tests

---
+++ src/app.back/EpicRoadTrip.Test/Application/Authentications/AuthenticationServiceTest.cs
@@ -0,0 +1,215 @@
+﻿using EpicRoadTrip.Application.Authentications;
+using EpicRoadTrip.Application.Options;
+using EpicRoadTrip.Domain.Users;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace EpicRoadTrip.Test.Application.Authentications;
+
+[TestClass]
+public class AuthenticationServiceTest
+{
+    private AuthenticationService _authenticationService;
+    private readonly Mock<IConfiguration> _configurationMock = new();
+    private readonly Mock<IAuthenticationRepository> _authenticationRepositoryMock = new();
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _authenticationService = new AuthenticationService(_configurationMock.Object, _authenticationRepositoryMock.Object);
+    }
+
+    [TestMethod]
+    public async Task IsEmailAlreadyUse_ShouldReturnTrue()
+    {
+        // Arrange
+        const string email = "test@example.com";
+
+        // Act
+        var result = await _authenticationService.IsEmailAlreadyUse(email, CancellationToken.None);
+        
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void Encrypt_ShouldReturnResultSuccess()
+    {
+        // Arrange
+        const string content = "password";
+        const string encodingKey = "0123456789ABCDEF";
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(EncodingKey))[nameof(EncodingKey.Value)])
+            .Returns(encodingKey);
+
+        // Act
+        var result = _authenticationService.Encrypt(content);
+
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+    }
+
+    [TestMethod]
+    public void Encrypt_ShouldReturnResultFailure_WithEmptyEncodingKey()
+    {
+        // Arrange
+        const string content = "password";
+        const string encodingKey = "";
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(EncodingKey))[nameof(EncodingKey.Value)])
+            .Returns(encodingKey);
+
+        // Act
+        var result = _authenticationService.Encrypt(content);
+
+        // Assert
+        Assert.IsFalse(result.IsSuccess);
+    }
+
+    [TestMethod]
+    public void Encrypt_SHouldReturnResultFailure_WithInvalidEncodingKeyLength()
+    {
+        // Arrange
+        const string content = "password";
+        const string encodingKey = "0123456789";
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(EncodingKey))[nameof(EncodingKey.Value)])
+            .Returns(encodingKey);
+
+        // Act
+        var result = _authenticationService.Encrypt(content);
+
+        // Assert
+        Assert.IsFalse(result.IsSuccess);
+    }
+
+    [TestMethod]
+    public void HashWithSalt_ShouldReturnResultSuccess()
+    {
+        // Arrange
+        const string content = "password";
+        const string salt = "0123456789ABCDEF";
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(Salt))[nameof(Salt.Value)])
+            .Returns(salt);
+
+        // Act
+        var result = _authenticationService.HashWithSalt(content);
+
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+    }
+
+    [TestMethod]
+    public void HashWithSalt_ShouldReturnResultFailure_WithEmptySalt()
+    {
+        // Arrange
+        const string content = "password";
+        const string salt = "";
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(Salt))[nameof(Salt.Value)])
+            .Returns(salt);
+
+        // Act
+        var result = _authenticationService.HashWithSalt(content);
+
+        // Assert
+        Assert.IsFalse(result.IsSuccess);
+    }
+
+    [TestMethod]
+    public void GenerateToken_ShouldReturnResultSuccess()
+    {
+        // Arrange
+        const string issuer = "issuer";
+        const string audience = "audience";
+        const string key = "4F8A760BCD2E09A1E3F514C68E4F37D9";
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(Jwt))[nameof(Jwt.Issuer)])
+            .Returns(issuer);
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(Jwt))[nameof(Jwt.Audience)])
+            .Returns(audience);
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(Jwt))[nameof(Jwt.Key)])
+            .Returns(key);
+
+        const string firstName = "John";
+        const string lastName = "Doe";
+        const string email = "test@gmail.com";
+        const string password = "password";
+        const int age = 25;
+        const bool gender = true;
+        var user = User.Create(firstName, lastName, email, password, age, gender);
+
+        // Act
+        var result = _authenticationService.GenerateToken(user.Value);
+
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+    }
+
+    [TestMethod]
+    public void GenerateToken_ShouldReturnResultFailure_WithEmptyIssuerOrAudianceOrKey()
+    {
+        // Arrange
+        const string issuer = "";
+        const string audience = "";
+        const string key = "";
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(Jwt))[nameof(Jwt.Issuer)])
+            .Returns(issuer);
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(Jwt))[nameof(Jwt.Audience)])
+            .Returns(audience);
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(Jwt))[nameof(Jwt.Key)])
+            .Returns(key);
+
+        const string firstName = "John";
+        const string lastName = "Doe";
+        const string email = "test@gmail.com";
+        const string password = "password";
+        const int age = 25;
+        const bool gender = true;
+        var user = User.Create(firstName, lastName, email, password, age, gender);
+
+        // Act
+        var result = _authenticationService.GenerateToken(user.Value);
+
+        // Assert
+        Assert.IsFalse(result.IsSuccess);
+    }
+
+    [TestMethod]
+    public void GenerateToken_ShouldReturnResultFailure_WithWrongKey()
+    {
+        // Arrange
+        const string issuer = "issuer";
+        const string audience = "audience";
+        const string key = "test";
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(Jwt))[nameof(Jwt.Issuer)])
+            .Returns(issuer);
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(Jwt))[nameof(Jwt.Audience)])
+            .Returns(audience);
+        _configurationMock
+            .Setup(x => x.GetSection(nameof(Jwt))[nameof(Jwt.Key)])
+            .Returns(key);
+
+        const string firstName = "John";
+        const string lastName = "Doe";
+        const string email = "test@gmail.com";
+        const string password = "password";
+        const int age = 25;
+        const bool gender = true;
+        var user = User.Create(firstName, lastName, email, password, age, gender);
+
+        // Act
+        var result = _authenticationService.GenerateToken(user.Value);
+
+        // Assert
+        Assert.IsFalse(result.IsSuccess);
+    }
+}

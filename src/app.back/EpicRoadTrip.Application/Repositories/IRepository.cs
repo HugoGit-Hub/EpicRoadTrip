16ac046c04311fff945e2453b997b70453bf2737
hugo <hugo.decuq.34@icloud.com>
2024-03-09 09:29:06 +0100

    HDO: Create generic repository

---
+++ src/app.back/EpicRoadTrip.Application/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using EpicRoadTrip.Domain.ErrorHandling;
+
+namespace EpicRoadTrip.Application.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    public Task<Result<T>> Create(T entity, CancellationToken cancellationToken);
+
+    public Task<Result<T>> Update(T entity, CancellationToken cancellationToken);
+
+    public Task<Result<T>> Delete(T entity, CancellationToken cancellationToken);
+
+    public Task<Result<T>> GetById(int id, CancellationToken cancellationToken);
+
+    public Result<IEnumerable<T>> GetAll();
+}

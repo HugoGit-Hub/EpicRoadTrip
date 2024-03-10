394947e4aa7554894a1b2ea33ae0f59351a309d6
hugo <hugo.decuq.34@icloud.com>
2024-03-10 18:03:06 +0100

    HDO: Create CurrentUser service

---
+++ src/app.back/EpicRoadTrip.Application/CurrentUsers/CurrentUserService.cs
@@ -0,0 +1,43 @@
+﻿using EpicRoadTrip.Domain.CurrentUsers;
+using EpicRoadTrip.Domain.ErrorHandling;
+using EpicRoadTrip.Domain.Users;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace EpicRoadTrip.Application.CurrentUsers;
+
+public class CurrentUserService(
+    IHttpContextAccessor httpContextAccessor,
+    IUserService userService) 
+    : ICurrentUserService
+{
+    private ClaimsPrincipal CurrentUser => httpContextAccessor.HttpContext.User;
+
+    public async Task<Result<User>> GetCurrentUser(CancellationToken cancellationToken)
+    {
+        var email = GetClaim(ClaimTypes.Email);
+        if (email.IsFailure)
+        {
+            return Result<User>.Failure(email.Error);
+        }
+
+        var currentUser = await userService.GetByEmail(email.Value, cancellationToken);
+        if (currentUser.IsFailure)
+        {
+            return Result<User>.Failure(currentUser.Error);
+        }
+        
+        return currentUser.IsFailure 
+            ? Result<User>.Failure(currentUser.Error) 
+            : Result<User>.Success(currentUser.Value);
+    }
+
+    private Result<string> GetClaim(string claimType)
+    {
+        var result = CurrentUser.Claims.FirstOrDefault(c => c.Type == claimType)?.Value;
+
+        return result is null
+            ? Result<string>.Failure(CurrentUserErrors.ClaimTypeNullError)
+            : Result<string>.Success(result);
+    }
+}
---
+++ src/app.back/EpicRoadTrip.Domain/CurrentUsers/ICurrentUserService.cs
@@ -0,0 +1,9 @@
+﻿using EpicRoadTrip.Domain.ErrorHandling;
+using EpicRoadTrip.Domain.Users;
+
+namespace EpicRoadTrip.Domain.CurrentUsers;
+
+public interface ICurrentUserService
+{
+    public Task<Result<User>> GetCurrentUser(CancellationToken cancellationToken);
+}

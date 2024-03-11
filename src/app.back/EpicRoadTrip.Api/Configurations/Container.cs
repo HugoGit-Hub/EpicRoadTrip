38277b0f438421c985a5dbbad08e90e0e75338e9
lucas066001 <chapuis863@gmail.com>
2024-03-11 16:33:52 +0100

    LCS: Including missing methods

--- src/app.back/EpicRoadTrip.Api/Configurations/Container.cs
+++ src/app.back/EpicRoadTrip.Api/Configurations/Container.cs
@@ -57,7 +57,9 @@ public static class Container
                               builder =>
                               {
                                   builder.WithOrigins("http://127.0.0.1:5173", "https://127.0.0.1:5173",
-                                                    "http://localhost:5173", "https://localhost:5173");
+                                                    "http://localhost:5173", "https://localhost:5173")
+                                          .AllowAnyMethod()
+                                          .AllowAnyHeader();
                               });
         });
 

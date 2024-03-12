3aee0839fe2965724772de06fdfc6d5db0a97136
hugo <hugo.decuq.34@icloud.com>
2024-03-12 17:36:21 +0100

    HDO: Add delete route work flow

---
+++ src/app.back/EpicRoadTrip.Application/Routes/DeleteRoute/DeleteRouteCommand.cs
@@ -0,0 +1,6 @@
+﻿using EpicRoadTrip.Domain.ErrorHandling;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Routes.DeleteRoute;
+
+public record DeleteRouteCommand(int Id) : IRequest<Result<DeleteRouteResponse>>;

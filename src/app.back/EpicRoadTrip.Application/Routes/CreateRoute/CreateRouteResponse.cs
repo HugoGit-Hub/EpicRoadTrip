51471be8c49db044501fae5a4ccb828135055a94
hugo <hugo.decuq.34@icloud.com>
2024-03-12 08:54:17 +0100

    HDO: Add create route work flow

---
+++ src/app.back/EpicRoadTrip.Application/Routes/CreateRoute/CreateRouteResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace EpicRoadTrip.Application.Routes.CreateRoute;
+
+public record CreateRouteResponse
+{
+    public int Id { get; init; }
+
+    public double Distance { get; init; }
+
+    public TimeSpan Duration { get; init; }
+
+    public int CityOneId { get; init; }
+
+    public int CityTwoId { get; init; }
+
+    public int RoadtripId { get; init; }
+}

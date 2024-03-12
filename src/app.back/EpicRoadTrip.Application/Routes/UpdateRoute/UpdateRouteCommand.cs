f97f98e0dc938584fede1ccb9d5ccab0ffa56cd7
hugo <hugo.decuq.34@icloud.com>
2024-03-12 17:17:55 +0100

    HDO: Add update route work flow

---
+++ src/app.back/EpicRoadTrip.Application/Routes/UpdateRoute/UpdateRouteCommand.cs
@@ -0,0 +1,6 @@
+﻿using EpicRoadTrip.Domain.ErrorHandling;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Routes.UpdateRoute;
+
+public record UpdateRouteCommand(UpdateRouteRequest Request) : IRequest<Result<UpdateRouteResponse>>;
---
+++ src/app.back/EpicRoadTrip.Application/Routes/UpdateRoute/UpdateRouteCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using EpicRoadTrip.Application.Repositories;
+using EpicRoadTrip.Domain.ErrorHandling;
+using EpicRoadTrip.Domain.Routes;
+using Mapster;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Routes.UpdateRoute;
+
+public class UpdateRouteCommandHandler(IRepository<Route> repository)
+    : IRequestHandler<UpdateRouteCommand, Result<UpdateRouteResponse>>
+{
+    public async Task<Result<UpdateRouteResponse>> Handle(UpdateRouteCommand command, CancellationToken cancellationToken)
+    {
+        var request = command.Request;
+        var timeSpan = TimeSpan.FromTicks(request.Duration);
+        var route = Route.Create(
+            request.Id,
+            request.Distance,
+            timeSpan,
+            request.CityOneId,
+            request.CityTwoId,
+            request.RoadtripId);
+        if (route.IsFailure)
+        {
+            return Result<UpdateRouteResponse>.Failure(route.Error);
+        }
+
+        var update = await repository.Update(route.Value, cancellationToken);
+        var updateRouteResponse = update.Value.Adapt<UpdateRouteResponse>();
+        
+        return update.IsFailure 
+            ? Result<UpdateRouteResponse>.Failure(update.Error) 
+            : Result<UpdateRouteResponse>.Success(updateRouteResponse);
+    }
+}

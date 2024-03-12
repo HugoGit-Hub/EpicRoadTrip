4eff1d998f046c7169354459b2d0321f803b3470
hugo <hugo.decuq.34@icloud.com>
2024-03-12 08:55:01 +0100

    HDO: Add read route work flow

---
+++ src/app.back/EpicRoadTrip.Application/Routes/GetAllRoute/GetAllRouteQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using EpicRoadTrip.Application.Repositories;
+using EpicRoadTrip.Domain.ErrorHandling;
+using EpicRoadTrip.Domain.Routes;
+using Mapster;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Routes.GetAllRoute;
+
+public class GetAllRouteQueryHandler(IRepository<Route> repository)
+    : IRequestHandler<GetAllRouteQuery, Result<IEnumerable<GetAllRouteResponse>>>
+{
+    public async Task<Result<IEnumerable<GetAllRouteResponse>>> Handle(GetAllRouteQuery query, CancellationToken cancellationToken)
+    {
+        var getAll = repository.GetAll();
+        var getAllRouteResponse = getAll.Value.Adapt<IEnumerable<GetAllRouteResponse>>();
+        
+        return getAll.IsFailure
+            ? Result<IEnumerable<GetAllRouteResponse>>.Failure(getAll.Error)
+            : Result<IEnumerable<GetAllRouteResponse>>.Success(getAllRouteResponse);
+    }
+}
---
+++ src/app.back/EpicRoadTrip.Application/Routes/GetRoute/GetRouteQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using EpicRoadTrip.Application.Repositories;
+using EpicRoadTrip.Domain.ErrorHandling;
+using EpicRoadTrip.Domain.Routes;
+using Mapster;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Routes.GetRoute;
+
+public class GetRouteQueryHandler(IRepository<Route> repository)
+    : IRequestHandler<GetRouteQuery, Result<GetRouteResponse>>
+{
+    public async Task<Result<GetRouteResponse>> Handle(GetRouteQuery request, CancellationToken cancellationToken)
+    {
+        var route = await repository.GetById(request.Id, cancellationToken);
+        var getRouteResponse = route.Value.Adapt<GetRouteResponse>();
+
+        return route.IsFailure 
+            ? Result<GetRouteResponse>.Failure(route.Error) 
+            : Result<GetRouteResponse>.Success(getRouteResponse);
+    }
+}

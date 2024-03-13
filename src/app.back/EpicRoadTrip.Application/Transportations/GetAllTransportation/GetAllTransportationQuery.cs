9377fcdd8aa117f47edc98bec9a118518b18eede
hugo <hugo.decuq.34@icloud.com>
2024-03-13 11:38:05 +0100

    HDO: Add read transportation work flow

---
+++ src/app.back/EpicRoadTrip.Application/Transportations/GetAllTransportation/GetAllTransportationQuery.cs
@@ -0,0 +1,7 @@
+﻿using EpicRoadTrip.Application.Transportations.GetTransportation;
+using EpicRoadTrip.Domain.ErrorHandling;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Transportations.GetAllTransportation;
+
+public record GetAllTransportationQuery : IRequest<Result<IEnumerable<GetTransportationResponse>>>;
---
+++ src/app.back/EpicRoadTrip.Application/Transportations/GetTransportation/GetTransportationQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using EpicRoadTrip.Application.Repositories;
+using EpicRoadTrip.Domain.ErrorHandling;
+using EpicRoadTrip.Domain.Transportations;
+using Mapster;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Transportations.GetTransportation;
+
+public class GetTransportationQueryHandler(IRepository<Transportation> repository)
+    : IRequestHandler<GetTransportationQuery, Result<GetTransportationResponse>>
+{
+    public async Task<Result<GetTransportationResponse>> Handle(GetTransportationQuery query, CancellationToken cancellationToken)
+    {
+        var get = await repository.GetById(query.Id, cancellationToken);
+        var getTransportationResponse = get.Value.Adapt<GetTransportationResponse>();
+        
+        return get.IsFailure 
+            ? Result<GetTransportationResponse>.Failure(get.Error) 
+            : Result<GetTransportationResponse>.Success(getTransportationResponse);
+    }
+}

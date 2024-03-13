528e51ee604acbe48dbb397585019de4c6bc731f
hugo <hugo.decuq.34@icloud.com>
2024-03-13 17:09:36 +0100

    HDO: Add read city work flow

---
+++ src/app.back/EpicRoadTrip.Application/Cities/GetAllCity/GetAllCityQuery.cs
@@ -0,0 +1,7 @@
+﻿using EpicRoadTrip.Application.Cities.GetCity;
+using EpicRoadTrip.Domain.ErrorHandling;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Cities.GetAllCity;
+
+public record GetAllCityQuery : IRequest<Result<IEnumerable<GetCityResponse>>>;
---
+++ src/app.back/EpicRoadTrip.Application/Cities/GetCity/GetCityQuery.cs
@@ -0,0 +1,6 @@
+﻿using EpicRoadTrip.Domain.ErrorHandling;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Cities.GetCity;
+
+public record GetCityQuery(int Id) : IRequest<Result<GetCityResponse>>;

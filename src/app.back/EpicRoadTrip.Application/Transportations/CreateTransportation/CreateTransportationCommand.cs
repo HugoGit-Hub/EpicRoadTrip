fdfe4859777fd0608f54cf8efc43ecb26389be0a
hugo <hugo.decuq.34@icloud.com>
2024-03-13 10:47:42 +0100

    HDO: Add create transportation work flow

---
+++ src/app.back/EpicRoadTrip.Application/Transportations/CreateTransportation/CreateTransportationCommand.cs
@@ -0,0 +1,6 @@
+﻿using EpicRoadTrip.Domain.ErrorHandling;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Transportations.CreateTransportation;
+
+public record CreateTransportationCommand(CreateTransportationRequest Request) : IRequest<Result<CreateTransportationResponse>>;
---
+++ src/app.back/EpicRoadTrip.Application/Transportations/CreateTransportation/CreateTransportationResponse.cs
@@ -0,0 +1,20 @@
+﻿using EpicRoadTrip.Domain.Transportations;
+
+namespace EpicRoadTrip.Application.Transportations.CreateTransportation;
+
+public record CreateTransportationResponse
+{
+    public int Id { get; init; }
+
+    public double Cost { get; init; }
+
+    public double Score { get; init; }
+
+    public required string Company { get; init; }
+
+    public required string Address { get; init; }
+
+    public TransportationType TransportationType { get; init; }
+
+    public int RouteId { get; init; }
+}

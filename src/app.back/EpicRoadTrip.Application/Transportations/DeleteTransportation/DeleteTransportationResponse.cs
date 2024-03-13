abdd36aede7909ee206de9aeb2361e362a9b2308
hugo <hugo.decuq.34@icloud.com>
2024-03-13 12:16:40 +0100

    HDO: Add delete transportation work flow

---
+++ src/app.back/EpicRoadTrip.Application/Transportations/DeleteTransportation/DeleteTransportationResponse.cs
@@ -0,0 +1,20 @@
+﻿using EpicRoadTrip.Domain.Transportations;
+
+namespace EpicRoadTrip.Application.Transportations.DeleteTransportation;
+
+public record DeleteTransportationResponse
+{
+    public int Id { get; init; }
+
+    public double Cost { get; init; }
+
+    public double Score { get; init; }
+
+    public required string Company { get; init; }
+
+    public required string Address { get; init; }
+
+    public TransportationType TransportationType { get; init; }
+
+    public int RouteId { get; init; }
+}

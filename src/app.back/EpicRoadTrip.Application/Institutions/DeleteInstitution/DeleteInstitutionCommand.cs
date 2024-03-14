903a93430427613937c4cedad41936e4e7b941d1
hugo <hugo.decuq.34@icloud.com>
2024-03-14 09:22:44 +0100

    HDO: Add delete institution work flow

---
+++ src/app.back/EpicRoadTrip.Application/Institutions/DeleteInstitution/DeleteInstitutionCommand.cs
@@ -0,0 +1,6 @@
+﻿using EpicRoadTrip.Domain.ErrorHandling;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Institutions.DeleteInstitution;
+
+public record DeleteInstitutionCommand(int Id) : IRequest<Result<DeleteInstitutionResponse>>;
---
+++ src/app.back/EpicRoadTrip.Application/Institutions/DeleteInstitution/DeleteInstitutionCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using EpicRoadTrip.Application.Repositories;
+using EpicRoadTrip.Domain.ErrorHandling;
+using EpicRoadTrip.Domain.Institutions;
+using Mapster;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Institutions.DeleteInstitution;
+
+public class DeleteInstitutionCommandHandler(IRepository<Institution> repository)
+    : IRequestHandler<DeleteInstitutionCommand, Result<DeleteInstitutionResponse>>
+{
+    public async Task<Result<DeleteInstitutionResponse>> Handle(DeleteInstitutionCommand command, CancellationToken cancellationToken)
+    {
+        var institution = await repository.GetById(command.Id, cancellationToken);
+        if (institution.IsFailure)
+        {
+            return Result<DeleteInstitutionResponse>.Failure(institution.Error);
+        }
+
+        var delete = await repository.Delete(institution.Value, cancellationToken);
+        if (delete.IsFailure)
+        {
+            return Result<DeleteInstitutionResponse>.Failure(delete.Error);
+        }
+
+        var response = delete.Value.Adapt<DeleteInstitutionResponse>();
+
+        return Result<DeleteInstitutionResponse>.Success(response);
+    }
+}

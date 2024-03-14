205e7959228ce258e291051d90d555bd76894b3e
hugo <hugo.decuq.34@icloud.com>
2024-03-14 09:22:30 +0100

    HDO: Add create institution work flow

---
+++ src/app.back/EpicRoadTrip.Application/Institutions/CreateInstitution/CreateInstitutionCommand.cs
@@ -0,0 +1,6 @@
+﻿using EpicRoadTrip.Domain.ErrorHandling;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Institutions.CreateInstitution;
+
+public record CreateInstitutionCommand(CreateInstitutionRequest Request) : IRequest<Result<CreateInstitutionResponse>>;
---
+++ src/app.back/EpicRoadTrip.Application/Institutions/CreateInstitution/CreateInstitutionCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using EpicRoadTrip.Application.Repositories;
+using EpicRoadTrip.Domain.ErrorHandling;
+using EpicRoadTrip.Domain.Institutions;
+using Mapster;
+using MediatR;
+
+namespace EpicRoadTrip.Application.Institutions.CreateInstitution;
+
+public class CreateInstitutionCommandHandler(IRepository<Institution> repository)
+    : IRequestHandler<CreateInstitutionCommand, Result<CreateInstitutionResponse>>
+{
+    public async Task<Result<CreateInstitutionResponse>> Handle(CreateInstitutionCommand command, CancellationToken cancellationToken)
+    {
+        var resquest = command.Request;
+        var institution = Institution.Create(
+            0,
+            resquest.Name,
+            resquest.Price,
+            resquest.PhoneNumber,
+            resquest.Email,
+            resquest.Address,
+            resquest.Type,
+            resquest.CityId);
+        if (institution.IsFailure)
+        {
+            return Result<CreateInstitutionResponse>.Failure(institution.Error);
+        }
+
+        var create = await repository.Create(institution.Value, cancellationToken);
+        if (create.IsFailure)
+        {
+            return Result<CreateInstitutionResponse>.Failure(create.Error);
+        }
+
+        var response = create.Value.Adapt<CreateInstitutionResponse>();
+
+        return Result<CreateInstitutionResponse>.Success(response);
+    }
+}

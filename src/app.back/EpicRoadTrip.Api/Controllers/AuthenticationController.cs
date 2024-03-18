e1b1cd44bd6c6c61e92bd2055ef8859e4ab5aec1
hugo <hugo.decuq.34@icloud.com>
2024-03-18 10:23:56 +0100

    HDO: Adapt endpoints

--- src/app.back/EpicRoadTrip.Api/Controllers/AuthenticationController.cs
+++ src/app.back/EpicRoadTrip.Api/Controllers/AuthenticationController.cs
@@ -13,7 +13,7 @@ public class AuthenticationController(ISender sender) : Controller
 {
     [AllowAnonymous]
     [HttpPost(nameof(Register))]
-    public async Task<ActionResult<string>> Register(RegisterRequest request, CancellationToken cancellationToken)
+    public async Task<ActionResult<RegisterResponse>> Register(RegisterRequest request, CancellationToken cancellationToken)
     {
         var result = await sender.Send(new RegisterCommand(request), cancellationToken);
         if (result.IsFailure)
@@ -26,7 +26,7 @@ public class AuthenticationController(ISender sender) : Controller
 
     [AllowAnonymous]
     [HttpPost(nameof(Login))]
-    public async Task<ActionResult<string>> Login(LoginRequest request, CancellationToken cancellationToken)
+    public async Task<ActionResult<LoginResponse>> Login(LoginRequest request, CancellationToken cancellationToken)
     {
         var result = await sender.Send(new LoginCommand(request), cancellationToken);
         if (result.Error == AuthenticationErrors.InvalidEmailOrPasswordError)
